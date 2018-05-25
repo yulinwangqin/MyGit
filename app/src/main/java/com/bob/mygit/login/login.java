81476db6fa0c225626c8d8ac6f39d0223b2214c5
yulinwangqin <39329512+yulinwangqin@users.noreply.github.com>
2018-05-25 17:46:53 +0800

    第一次

---
+++ app/src/main/java/com/bob/mygit/login/login.java
@@ -0,0 +1,14 @@
+package com.bob.mygit.login;
+
+public class login {
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+}
